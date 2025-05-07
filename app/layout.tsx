bb821413ed1ca8e34785c6547b66d4f5c50c8076
Tsukoyachi <Axel.delille31@gmail.com>
2025-05-07 11:42:24 +0200

    feat: Addition of canonical tag and shorten description for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
 import type { Metadata } from 'next';
 import { Rowdies } from 'next/font/google';
 import './globals.css';
-import { useRouter } from 'next/navigation';
 import Head from 'next/head';
 
 const rowdies = Rowdies({ weight: ['300', '400', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
+    metadataBase: new URL('https://jondepannage.tsukoyachi.ovh'),
     title: 'JonDepannage - Dépannage, Remorquage, Transport 24h/24 7j/7',
-    description: "JonDepannage propose des services de dépannage, remorquage et transport d'urgence 24h/24 et 7j/7 dans toute la région Provence-Alpes-Côte d'Azur. Interventions rapides et professionnelles pour tous vos besoins de mobilité, avec un service fiable et efficace.",
+    description: "JonDepannage propose dépannage, remorquage et transport d'urgence 24h/24, 7j/7 en région PACA, avec des interventions rapides, fiables et efficaces.",
+    alternates: {
+        canonical: '/',
+        languages: {
+            'fr': '/fr',
+            'en': '/en',
+        },
+    }
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    const canonicalUrl = 'https://jondepannage.tsukoyachi.ovh/';
 
     return (
         <html lang="fr">
-            <head>
-                <Head>
-                    <link rel="canonical" href={canonicalUrl} />
-                </Head>
-            </head>
             <body className={rowdies.className}>{children}</body>
         </html>
     );
